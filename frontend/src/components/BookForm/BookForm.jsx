5e41c4c783ec5b7fb1b5700f5017c21a77d14c0b
Timofey <pttima@yandex.ru>
2023-11-23 23:37:17 +0300

    Added spinner on the add random via api button

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { RiLoopRightLine } from "react-icons/ri";
 import { addBook, fetchBook } from '../../redux/slices/booksSlice'
 import { setError } from '../../redux/slices/errorSlice'
 import './BookForm.css'
@@ -12,6 +13,7 @@ const BookForm = () => {
 
    const [title, setTitle] = useState('')
    const [author, setAuthor] = useState('')
+   const [isLoading, setIsLoading] = useState(false)
 
    const submitHandler = (event) => {
       event.preventDefault()
@@ -35,8 +37,14 @@ const BookForm = () => {
       dispatch(addBook(randomBookWithId))
    }
 
-   const addRandomBookViaAPIHandler = () => {
-      return dispatch(fetchBook('http://localhost:4000/random-book'))
+   const addRandomBookViaAPIHandler = async () => {
+      try {
+         setIsLoading(true)
+         await dispatch(fetchBook('http://localhost:4000/random-book-delayed'))
+      } finally {
+         setIsLoading(false)
+      }
+
    }
 
    return (
@@ -53,7 +61,13 @@ const BookForm = () => {
             </div>
             <button type='submit'>Add Book</button>
             <button type='button' onClick={addRandomBookHandler}>Add Random Book</button>
-            <button type='button' onClick={addRandomBookViaAPIHandler}>Add Random via API</button>
+            <button type='button' onClick={addRandomBookViaAPIHandler} disabled={isLoading}>
+               {isLoading
+                  ? (<><span>Loading Book...</span>
+                     <RiLoopRightLine className='spinner' />
+                  </>)
+                  : 'Add Random via API'}
+            </button>
          </form>
       </div>
    )
